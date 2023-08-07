682067521c57b9d5786d15182ebb1a246cecf9cb
FranCabrera <cabrerafranconehuen@gmail.com>
2023-08-07 16:14:45 -0300

    Actualizaciones para la 3er entrega

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 
-alert("Esta página va a poder calcular el precio total de los productos seleccionados por el usuario (como un carrito de compras), por ahora lo unico que puede hacer es agregar los productos y los precios de forma manual.");
+//alert("Esta página va a poder calcular el precio total de los productos seleccionados por el usuario (como un carrito de compras), por ahora lo unico que puede hacer es agregar los productos y los precios de forma manual.");
+
+localStorage.setItem("1", 1000)
+localStorage.setItem("2", 2000)
+localStorage.setItem("3", 3000)
+localStorage.setItem("4", 4000)
+localStorage.setItem("5", 5000)
+localStorage.setItem("6", 6000)
 
 const lista_productos = {
-  'Mesa': 1000,
-  'Silla': 2000,
-  'Caja': 3000
+  '1': 1000,
+  '2': 2000,
+  '3': 3000,
+  '4': 4000,
+  '5': 5000,
+  '6': 6000
 };
 
 function getPrecio(prod) {
@@ -13,7 +23,7 @@ function getPrecio(prod) {
 
 let productos = []
 let total = 0
-
+/*
 function carrito(){
   alert("Mesa $1000 \nSilla $2000 \nCaja $3000") 
   let cant = parseInt(prompt("Ingrese la cantidad de productos que quiera agregar: "))
@@ -36,5 +46,41 @@ function eliminar(){
   total -= getPrecio(prod)
   alert("Ahora los productos cargados son: " + productos + "\nEl total de la compra es de: " + total)
 }
+*/
+
+//document.getElementById("agregar").addEventListener("click", function(event) {
+//  alert("Se clickeo xd");
+//  event.preventDefault();
+//});
+const botones = document.querySelectorAll(".botones");
+
+function eliminar(){
+  const cuandoSeHaceClick = function (evento) {
+    const valor = localStorage.getItem(this.value)
+    const prod = this.id
+    total -= valor
+    alert("el total es de: " + total + productos)
+  }
+  botones.forEach(boton => {
+    boton.addEventListener("click", cuandoSeHaceClick);
+  });
+}
+
+function carrito(){
+  const cuandoSeHaceClick = function (evento) {
+    const valor = localStorage.getItem(this.value)
+    const prod = this.id
+    productos.push(prod)
+    total += valor
+    alert("el total es de: " + total + productos)
+  }
+  botones.forEach(boton => {
+    boton.addEventListener("click", cuandoSeHaceClick);
+  });
+
+  document.getElementById("eliminar").addEventListener("click", eliminar);
+}
+
+
 
 carrito()
